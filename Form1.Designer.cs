054e79ebb74bc280dc3fe7376166b6a8fc7721c4
akrem <akrem@NIKO>
2023-02-10 19:53:11 +0400

    update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(40, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(203, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Students Name And Surname";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(277, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(511, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Group";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(511, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Term";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private ComboBox comboBox1;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private Label label3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,50 @@
-namespace EntityFrameWork_Hw2
+using EntityFrameWork_Hw2.Context;
+
+namespace EntityFrameWork_Hw2;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+
+    private AppDbContext? AppDb { get; set; }
+    public Form1()
     {
-        public Form1()
+        InitializeComponent();
+        AppDb= new AppDbContext();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+            
+
+        foreach (var item in AppDb.Studentss)
         {
-            InitializeComponent();
+            comboBox1.Items.Add(item.FirstName);
+        }
+
+
+    }
+
+    private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+
+        var list = (from S in AppDb.Studentss
+                         where S.FirstName == comboBox1.Text
+                         select S.Id_Group).ToList();
+
+        foreach (var item in list)
+        {
+            textBox1.Text= item.ToString();
+        }
+
+        var list1 = (from S in AppDb.Studentss
+                    where S.FirstName == comboBox1.Text
+                    select S.Term).ToList();
+
+
+        foreach (var item in list1)
+        {
+            textBox2.Text = item.ToString();
         }
     }
 }
