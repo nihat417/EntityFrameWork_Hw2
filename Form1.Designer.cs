c55b93645c6e8ae91d0fcbb77bfb0f1c1ffee0f8
akrem <akrem@NIKO>
2023-02-10 21:11:21 +0400

    ///

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,6 +34,14 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -50,9 +58,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(40, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(203, 20);
+            this.label1.Size = new System.Drawing.Size(114, 20);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Students Name And Surname";
+            this.label1.Text = "Students Name ";
             // 
             // textBox1
             // 
@@ -63,7 +71,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(511, 40);
+            this.textBox2.Location = new System.Drawing.Point(450, 40);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(125, 27);
             this.textBox2.TabIndex = 3;
@@ -80,17 +88,93 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(511, 9);
+            this.label3.Location = new System.Drawing.Point(450, 9);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(42, 20);
             this.label3.TabIndex = 5;
             this.label3.Text = "Term";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(277, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Id_Dep";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(277, 146);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 27);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Teachers Name ";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(40, 146);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(203, 28);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(277, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Id_Author";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(277, 252);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 27);
+            this.textBox4.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 222);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(108, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Authors Name ";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(40, 252);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(203, 28);
+            this.comboBox3.TabIndex = 12;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
@@ -113,5 +197,13 @@
         private TextBox textBox2;
         private Label label2;
         private Label label3;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label6;
+        private ComboBox comboBox2;
+        private Label label5;
+        private TextBox textBox4;
+        private Label label7;
+        private ComboBox comboBox3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -21,6 +21,17 @@ public partial class Form1 : Form
             comboBox1.Items.Add(item.FirstName);
         }
 
+        foreach (var item in AppDb.Teacherss)
+        {
+            comboBox2.Items.Add(item.FirstName);
+        }
+
+
+        foreach (var item in AppDb.Categorys)
+        {
+            comboBox3.Items.Add(item.Id);
+        }
+
 
     }
 
@@ -46,5 +57,35 @@ public partial class Form1 : Form
         {
             textBox2.Text = item.ToString();
         }
+    }
+
+    private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        var list = (from S in AppDb.Teacherss
+                    where S.FirstName == comboBox2.Text
+                    select S.Id_Dep).ToList();
+
+        foreach (var item in list)
+        {
+            textBox3.Text = item.ToString();
+        }
+
+       
+    }
+
+    private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        var list = (from S in AppDb.Bookss
+                    where S.Name == comboBox3.Text
+                    select S.Id_Author).ToList();
+
+        foreach (var item in list)
+        {
+            textBox4.Text = item.ToString();
+        }
+
+        //var list1= (from S in AppDb.Authorss
+        //            where S.Id == list
+        //            select S.Id_Author).ToList();
     }
 }
